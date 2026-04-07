dc4fac014e8b90f51c23ff9db711f39aa90bcd09
Jhakhang <79897321+jhakhang@users.noreply.github.com>
2026-04-08 00:18:47 +0530

    added

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
+
+declare global {
+  interface Window {
+    __nanostores_player: any;
+    playerPlay: (payload: any) => void;
+    appNavigate: (href: string) => void;
+    __playerNowPlaying: any;
+  }
+}
